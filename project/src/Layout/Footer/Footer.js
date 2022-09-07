2f4fd59c008d3927fa040aeb9e329c891975af5d
Nooria Ehsani <103328903+Nooriaehsani@users.noreply.github.com>
2022-09-06 20:35:02 -0700

    Update Footer.js

--- project/src/Layout/Footer/Footer.js
+++ project/src/Layout/Footer/Footer.js
@@ -1,9 +1,38 @@
 
-import './Footer.css';
 const Footer = () => {
     return ( 
         <div className="footer" >
-          
+          <div className="top">
+            <h1>Thelookup</h1>
+            <p>Thelookup well-known API for address lookup and form valadation</p>
+       </div>
+       <div className="buttom">
+        <div className="section" id="sec-1"><p>Aboat</p>
+        <p>privacy policy</p>
+        <p>cookies policy</p>
+        <p>Terms of Use</p>
+        <p>our toturial</p> 
+        <p>Help and suport</p>
+        </div>
+        <div className="section">
+            <p>Adress lookup</p>
+            <p>Mobile varication</p>
+            <p>Bank veritifiction</p>
+            <p>Email valadation</p>
+            <p>Quaik start</p>
+            <p>Good pricing</p>
+            <p>Lookup tutorial</p>
+            </div>
+            <div className="section">
+                <h2> Address</h2>
+                <p>Sila Road,1st street</p>
+                <p>the lookup@gmail.com</p>
+                <p>0798827626</p>
+                <p>0798827626</p>
+                <p className="bottom">call now</p>
+            </div>
+       </div>
+
         </div>
      );
 }
