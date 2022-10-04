1f1316648f91fded314a6ad011b21768319ca56a
Khatima Sadat <86257958+KhatimaSadat@users.noreply.github.com>
2022-10-04 09:46:34 +0430

    Update AboutUs.js

--- project/src/Pages/AboutUs.js
+++ project/src/Pages/AboutUs.js
@@ -1,17 +1,4 @@
 
-import './AboutUs.css';
-const AboutUs = () => {
-    return ( 
-      <div className="about-us-gride">
-          
-      </div>
-     );
-}
- 
-export default AboutUs;
-
-
-
 
 
 
